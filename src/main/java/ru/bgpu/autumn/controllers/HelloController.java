f896bef4e5071a7f6868ba30e79eb4401544a9bd
out <piligrim.a3@gmail.com>
2026-03-18 12:46:48 +0900

    add: HelloService

--- src/main/java/ru/bgpu/autumn/controllers/HelloController.java
+++ src/main/java/ru/bgpu/autumn/controllers/HelloController.java
@@ -1,15 +1,21 @@
 package ru.bgpu.autumn.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import ru.bgpu.autumn.dto.HelloMessageDTO;
+import ru.bgpu.autumn.services.HelloService;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public  String index() {
-        return "Hello!";
+    @Autowired private HelloService helloService;
+
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    public HelloMessageDTO index(@PathVariable(name = "name") String name) {
+        return new HelloMessageDTO(name, helloService.message(name));
     }
 }
